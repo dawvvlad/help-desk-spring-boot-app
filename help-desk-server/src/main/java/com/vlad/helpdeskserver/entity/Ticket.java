83a6e6c9118f57a4828428b32b26429c827d809d
Vladislav <dawvvlad@users.noreply.github.com>
2024-07-14 20:04:14 +0300

    changed

--- help-desk-server/src/main/java/com/vlad/helpdeskserver/entity/Ticket.java
+++ help-desk-server/src/main/java/com/vlad/helpdeskserver/entity/Ticket.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+
+@Getter
+@Setter
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
 }
