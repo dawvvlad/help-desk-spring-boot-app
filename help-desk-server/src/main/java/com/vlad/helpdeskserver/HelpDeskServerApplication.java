34f34bfb81a9b460afbe2da8aef0f00dd0c1648f
dawvvlad <vgolikov1998@gmail.com>
2024-07-15 08:55:04 +0300

    update

--- help-desk-server/src/main/java/com/vlad/helpdeskserver/HelpDeskServerApplication.java
+++ help-desk-server/src/main/java/com/vlad/helpdeskserver/HelpDeskServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelpDeskServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDeskServerApplication.class, args);
 	}
-
 }
--- help-desk-server/src/main/java/com/vlad/helpdeskserver/config/SecurityConfig.java
+++ help-desk-server/src/main/java/com/vlad/helpdeskserver/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.vlad.helpdeskserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${active.directory.domain}")
+    private String adDomain;
+    @Value("${active.directory.ldap}")
+    private String adLdap;
+
+    @Bean
+    ActiveDirectoryLdapAuthenticationProvider authenticationProvider() {
+        return new ActiveDirectoryLdapAuthenticationProvider(adDomain, adLdap);
+    }
 }
