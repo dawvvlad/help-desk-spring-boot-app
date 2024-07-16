4008e18bad55b2cf8c3bde5a836d867beb1f8464
Vladislav <dawvvlad@users.noreply.github.com>
2024-07-16 13:06:47 +0300

    update

--- help-desk-server/src/main/java/com/vlad/helpdeskserver/controllers/ApiClientController.java
+++ help-desk-server/src/main/java/com/vlad/helpdeskserver/controllers/ApiClientController.java
@@ -26,4 +26,31 @@ public class ApiClientController {
         ticketService.create(ticketDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(ticketDTO);
     }
+
+
+/* test */
+//    //test
+//    @GetMapping("/info")
+//    public Map<String, Object> getCurrentUser(Authentication authentication) {
+//        Map<String, Object> userInfo = new HashMap<>();
+//        if(authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+//            userInfo.put("roles", authentication.getDetails());
+//            userInfo.put("info", authentication.getPrincipal());
+//            userInfo.put("g", authentication.getCredentials());
+//        }
+//        return userInfo;
+//    }
+//
+//
+//    //test
+//    @GetMapping("/hello")
+//    public Map<String, Object> hello(@AuthenticationPrincipal Person person) {
+//        Map<String, Object> userInfo = new HashMap<>();
+//        userInfo.put("cn", person.getCn());
+//        userInfo.put("dn", person.getSn());
+//        userInfo.put("sn", person.getDn());
+//        userInfo.put("authorities", person.getAuthorities());
+//        userInfo.put("description", person.getDescription());
+//        return userInfo;
+//    }
 }
