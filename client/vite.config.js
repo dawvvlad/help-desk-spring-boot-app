5d2b906eb6daa339bc77e7bb59a2fb5b06e80d96
dawvvlad <vgolikov1998@gmail.com>
2024-07-22 08:23:07 +0300

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../help-desk-server/src/main/resources/static', // Путь к папке сервера
+  }
 })
