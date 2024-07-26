515e3bc6e8e9a8c1de00fed89d6b22e472f2022f
dawvvlad <vgolikov1998@gmail.com>
2024-07-26 15:09:41 +0300

    updated

--- client/src/view/admin/settings/upload-order/UploadOrderSettings.jsx
+++ client/src/view/admin/settings/upload-order/UploadOrderSettings.jsx
@@ -1,14 +1,16 @@
 import {Link} from "react-router-dom";
 import {DateSelector} from "../../../../components/date-selector/DateSelector.jsx";
-
+import './upload-settings.css'
 
 export const UploadOrderSettings = () => {
 
     return (
         <>
             <div className={"container right-panel"}>
-                <Link to="/settings">Назад</Link>
-                <DateSelector/>
+                <div className={"upload-settings"}>
+                    <Link to="/settings">Назад</Link>
+                    <DateSelector/>
+                </div>
             </div>
         </>
     )
